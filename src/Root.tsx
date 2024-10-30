e5d91bb18ef22bc31bc8e64f0f9ac93ecb782c23
Joshua Horowitz <joshuah@alum.mit.edu>
2024-10-30 16:26:22 -0700

    prettier, etc

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,10 +1,10 @@
-import { memo } from 'react';
+import { memo } from "react";
 
 export const Root = memo(() => {
-  return <div className='prose p-6'>
-    <h1>my-vite</h1>
-    <p>
-      hi
-    </p>
-  </div>;
+  return (
+    <div className="prose p-6">
+      <h1>my-vite</h1>
+      <p>hi</p>
+    </div>
+  );
 });
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import { Root } from './Root.js';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Root } from "./Root.js";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path='/' element={<Root/>}/>
+        <Route path="/" element={<Root />} />
       </Routes>
     </HashRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
